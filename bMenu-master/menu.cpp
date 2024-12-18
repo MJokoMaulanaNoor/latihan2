e182ef0d09084e2d6d65e06ffff033708cfabdbb
MJokoMaulanaNoor <mjokomaulananoor@gmail.com>
2024-12-18 12:22:28 +0800

    u: penyesuaian isi pada halaman depan

--- bMenu-master/menu.cpp
+++ bMenu-master/menu.cpp
@@ -2,62 +2,52 @@
 #include <conio.h>
 using namespace std;
 
-
-void dMenu(){
-system("cls");
-cout<<"Aplikasi Tampilan Menu"<<"\n";       
-cout<<"1. Menu Pertama"<<"\n";            
-cout<<"2. Menu Kedua"<<"\n";            
-cout<<"3. Menu Ketiga"<<"\n";           
-cout<<"4. Menu Keempat"<<"\n";            
-cout<<"5. Exit"<<"\n";           
-cout<<"Masukan angka :";        
-
+// Fungsi untuk menampilkan menu
+void dMenu() {
+    system("cls");
+    cout << "Aplikasi Tampilan Menu" << "\n";
+    cout << "1. Menu Pertama" << "\n";
+    cout << "2. Menu Kedua" << "\n";
+    cout << "3. Menu Ketiga" << "\n";
+    cout << "4. Menu Keempat" << "\n";
+    cout << "5. Exit" << "\n";
+    cout << "Masukan angka: ";
 }
 
-void mPertama(string pesan){
-system("cls");
-cout<<"hallo saya menu "<<pesan;
-getch();
+// Fungsi untuk menampilkan menu pertama
+void mPertama(string pesan) {
+    system("cls");
+    cout << "Hallo saya menu " << pesan;
+    getch();  // Menunggu input dari pengguna untuk melanjutkan
 }
-
 
 int main() {
-char pl;
-do
-{
-    dMenu();
-    pl=getch();
-  switch (pl)
-  {
-   case '1':
-    /* code */
-    mPertama("pertama");
-    break;
-   case '2':
-    mPertama("ke- dua");
-    /* code */ 
-    break;  
-   case '3':
-    mPertama("ke- tiga");
-    /* code */
-    break;  
-   case '4':
-    mPertama("ke- empat");
-    /* code */
-    break;  
-  case '5':
-    /* code */
-    break;
-  
-  default:
-    system("cls");
-    cout<<"Pilihan Tidak Tersedia";
-    getch();
-    break;
-  }
-
-
-} while (pl!='5');
-  return 0;
+    char pl;
+    do {
+        dMenu();  // Menampilkan menu
+        pl = getch();  // Menunggu input dari pengguna
+        switch (pl) {
+            case '1':
+                mPertama("pertama");
+                break;
+            case '2':
+                mPertama("ke- dua");
+                break;
+            case '3':
+                mPertama("ke- tiga");
+                break;
+            case '4':
+                mPertama("ke- empat");
+                break;
+            case '5':
+                // Exit program
+                break;
+            default:
+                system("cls");
+                cout << "Pilihan Tidak Tersedia";
+                getch();  // Menunggu input sebelum melanjutkan
+                break;
+        }
+    } while (pl != '5');  // Loop sampai pengguna memilih untuk keluar
+    return 0;
 }
